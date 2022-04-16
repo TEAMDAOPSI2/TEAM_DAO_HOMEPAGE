7f6eb9df789bd6955cb72698de1cb56c5fb51065
Aditya Rizki Pratama <arizki717@gmail.com>
2022-04-16 12:28:46 +0700

    fix: move font injection to _document.jsx

--- pages/_app.js
+++ pages/_app.js
@@ -12,34 +12,8 @@ function MyApp({Component, pageProps}) {
                     sizes="32x32"
                     href="assets/round-team-logo.png"
                 />
-                <link
-                    rel="stylesheet"
-                    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css"
-                />
-                <link
-                    href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700;900&display=swap"
-                    rel="stylesheet"
-                />
-                <link
-                    href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap"
-                    rel="stylesheet"
-                />
-                <link
-                    rel="stylesheet"
-                    type="text/css"
-                    href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
-                />
-                <script
-                    async
-                    src="https://www.googletagmanager.com/gtag/js?id=G-2B1DBKSKZW"
-                />
-                <script>
-                    {`
-                        window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date()); gtag('config', 'G-2B1DBKSKZW');
-                    `}
-                </script>
+
+
             </Head>
             <Component {...pageProps} />
         </>
