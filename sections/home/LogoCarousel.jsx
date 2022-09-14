5ea4e8257f91c03af99833659dceeba36d3a8962
Aditya Rizki Pratama <arizki717@gmail.com>
2022-09-14 13:13:30 +0700

    update: change image  section

--- sections/home/LogoCarousel.jsx
+++ sections/home/LogoCarousel.jsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import styled from "styled-components";
 import Slider from "react-slick";
-import GamesGroup from "public/assets/game-section.png"
+import GamesGroup from "public/assets/Games-full.png"
 
 const cache = {};
 
