f6b1ff7296409f8a4dfdf05844c59e82087030a3
Aditya Rizki Pratama <arizki717@gmail.com>
2023-01-25 20:12:06 +0700

    fix:  make it better

--- pages/stakes.jsx
+++ pages/stakes.jsx
@@ -4,7 +4,6 @@ import FooterOld from "@components/FooterOld";
 import styled from "styled-components";
 import ListStakes from "@sections/stakes/ListStakes";
 import {useEffect, useState} from "react";
-import {io} from "socket.io-client";
 
 const MainDiv = styled.div`
   height: 1px;
@@ -12,13 +11,50 @@ const MainDiv = styled.div`
   padding-bottom: 120px;
 `;
 
-// const ListStakes = dynamic(() => import("@sections/stakes/ListStakes"), {
-//   ssr: false,
-// });
 
-const Stakes = () => {
+export async function getServerSideProps() {
+    const res = await fetch('https://stake.teamdao.app/api/matches')
+    const data = await res.json()
+    return {
+        props: {
+            data
+        }
+    }
+}
 
+const getBettors = (data) => {
+    const bettorsToday = data.today.map(match => match.bettors).flat();
+    const bettorsYesterday = data.yesterday.map(match => match.bettors).flat();
+    const bettorsTomorrow = data.tomorrow.map(match => match.bettors).flat();
+    const bettors = [...bettorsToday, ...bettorsYesterday, ...bettorsTomorrow];
 
+    // get latest bettors
+    const newestBettors = bettors.sort((a, b) => {
+        return new Date(b.timestamp) - new Date(a.timestamp);
+    }).slice(0, 10);
+
+    const getMatchDetails = (matchID) => {
+        return data.today.find(match => match.matchID === matchID) || data.yesterday.find(match => match.matchID === matchID) || data.tomorrow.find(match => match.matchID === matchID);
+    }
+
+    return newestBettors.map((bettor, index) => {
+        const match = getMatchDetails(bettor.matchID);
+        return {
+            ...bettor,
+            teamName: match.team[bettor.team].name,
+            category: match.type,
+            teams: Object.values(match.team).map(team => team.name),
+            odd: index % 2 === 0
+        }
+    });
+
+}
+
+const Stakes = ({data}) => {
+    const [stakes, setStakes] = useState([]);
+    useEffect(() => {
+        setStakes(getBettors(data));
+    }, [data]);
     return (
         <>
             <Head>
@@ -27,7 +63,7 @@ const Stakes = () => {
             <div>
                 <HeaderOld/>
                 <MainDiv>
-                    <ListStakes/>
+                    <ListStakes bettors={stakes} />
                 </MainDiv>
                 <FooterOld/>
             </div>
--- sections/stakes/ListStakes.jsx
+++ sections/stakes/ListStakes.jsx
@@ -1,190 +1,30 @@
 import React, {useContext, useEffect, useState} from "react";
 import {Table, FilterWrapper, GameWrapper} from "./style";
 import WSContext from "../../context/WSContext";
-import {faker} from "@faker-js/faker";
+import categoryStakes from "@data/stakeCategories.json";
+import EmojiConvertor from 'emoji-js';
 
-const randomMatch = [
-    {
-        userID: '1978772578',
-        name: '@TEAM_Beta_201????',
-        matchID: 'EQAGTV95O7OW',
-        teamID: '0PNFSUMR3YJ9',
-        team: 'b',
-        custom: false,
-        type: 'place-bet',
-        timestamp: '2023-01-14T12:04:10+08:00',
-        teamName: 'TEAM BDS',
-        teams: ['EXCEL ESPORTS', 'TEAM BDS'],
-        game: '🐉LOL',
-    },
-    {
-        userID: '1978772578',
-        name: '@TEAM_Beta_204 ????',
-        matchID: 'EQAGTV95O7OW',
-        teamID: '0PNFSUMR3YJ9',
-        team: 'b',
-        custom: false,
-        type: 'place-bet',
-        timestamp: '2023-01-14T12:04:10+08:00',
-        teamName: 'TEAM BDS',
-        teams: ['EXCEL ESPORTS', 'TEAM BDS'],
-        game: '🐉LOL',
-    },
-    {
-        userID: '1978772578',
-        name: '@TeamDAO???? | 1174 | ??GM INTERN (TEKKEN/ F1 )',
-        matchID: 'EQAGTV95O7OW',
-        teamID: '0PNFSUMR3YJ9',
-        team: 'a',
-        custom: false,
-        type: 'place-bet',
-        timestamp: '2023-01-22T23:59:50+08:00',
-        teamName: 'FULHAM',
-        teams: ['FULHAM', 'TOTTENHAM HOTSPUR'],
-        game: '⚽FOOTBALL',
-    },
-    {
-        userID: '1978772578',
-        name: '@TeamDAO???? | 5220 | GOLF15 | Natco',
-        matchID: 'EQAGTV95O7OW',
-        teamID: '0PNFSUMR3YJ9',
-        team: 'b',
-        custom: false,
-        type: 'place-bet',
-        timestamp: '2023-01-14T12:04:10+08:00',
-        teamName: 'TOTTENHAM HOTSPUR',
-        teams: ['FULHAM', 'TOTTENHAM HOTSPUR'],
-        game: '⚽FOOTBALL',
-    },
-    {
-        userID: '1978772578',
-        name: '@TeamDAO????| 1255 | ? ?e?? ??',
-        matchID: 'EQAGTV95O7OW',
-        teamID: '0PNFSUMR3YJ9',
-        team: 'b',
-        custom: false,
-        type: 'place-bet',
-        timestamp: "2023-01-23T11:03:09+08:00",
-        teamName: 'GAIMIN GLADIATORS',
-        teams: ['LIQUID', 'GAIMIN GLADIATORS'],
-        game: '🐲DOTA',
-    },
-    {
-        userID: '1978772578',
-        name: '@TeamDAO ???? | 1286 |??GM INTERN- KOF/TENNIS | Anne',
-        matchID: 'EQAGTV95O7OW',
-        teamID: '0PNFSUMR3YJ9',
-        team: 'a',
-        custom: false,
-        type: 'place-bet',
-        timestamp: "2023-01-23T11:03:09+08:00",
-        teamName: 'LIQUID',
-        teams: ['LIQUID', 'GAIMIN GLADIATORS'],
-        game: '🐲DOTA',
-    },
-    {
-        userID: '1978772578',
-        name: '@TeamDAO ???? | 2049 | BRAVO | DAO Jo1',
-        matchID: 'EQAGTV95O7OW',
-        teamID: '0PNFSUMR3YJ9',
-        team: 'a',
-        custom: false,
-        type: 'place-bet',
-        timestamp: "2023-01-23T11:04:14+08:00",
-        teamName: 'LIQUID',
-        teams: ['LIQUID', 'GAIMIN GLADIATORS'],
-        game: '🐲DOTA',
-    },
-    {
-        userID: '1978772578',
-        name: '@TeamDAO ???? | 4911 | COACH | YANKEE10 | Nico',
-        matchID: 'EQAGTV95O7OW',
-        teamID: '0PNFSUMR3YJ9',
-        team: 'a',
-        custom: false,
-        type: 'place-bet',
-        timestamp: "2023-01-23T11:04:14+08:00",
-        teamName: 'PISTONS',
-        teams: ['PISTONS', 'MILWAUKEE BUCKS'],
-        game: '🏀NBA',
-    },
-    {
-        userID: '1978772578',
-        name: '@TEAM_Beta_209 ????',
-        matchID: 'EQAGTV95O7OW',
-        teamID: '0PNFSUMR3YJ9',
-        team: 'a',
-        custom: false,
-        type: 'place-bet',
-        timestamp: "2023-01-23T13:26:00+08:00",
-        teamName: 'PISTONS',
-        teams: ['PISTONS', 'MILWAUKEE BUCKS'],
-        game: '🏀NBA',
-    },
-    {
-        userID: '1978772578',
-        name: '@TeamDAO ???? M88 | 1252 | ?? GM INTERN - MK | Aicel',
-        matchID: 'EQAGTV95O7OW',
-        teamID: '0PNFSUMR3YJ9',
-        team: 'a',
-        custom: false,
-        type: 'place-bet',
-        timestamp: "2023-01-23T11:04:14+08:00",
-        teamName: 'PISTONS',
-        teams: ['PISTONS', 'MILWAUKEE BUCKS'],
-        game: '🏀NBA',
-    },
-    {
-        userID: '1978772578',
-        name: '@TeamDAO ???? M88 | 1252 | ?? GM INTERN - MK | Aicel',
-        matchID: 'EQAGTV95O7OW',
-        teamID: '0PNFSUMR3YJ9',
-        team: 'a',
-        custom: false,
-        type: 'place-bet',
-        timestamp: "2023-01-23T13:47:31+08:00",
-        teamName: 'PISTONS',
-        teams: ['UTAH JAZZ', 'CHARLOTTE HORNETS'],
-        game: '🏀NBA',
-    },
-    {
-        userID: '1978772578',
-        name: '@TEAM_Beta_208 ????',
-        matchID: 'EQAGTV95O7OW',
-        teamID: '0PNFSUMR3YJ9',
-        team: 'a',
-        custom: false,
-        type: 'place-bet',
-        timestamp: "2023-01-23T13:47:31+08:00",
-        teamName: 'PISTONS',
-        teams: ['UTAH JAZZ', 'CHARLOTTE HORNETS'],
-        game: '🏀NBA',
-    }
-]
+const emojiConvertor = new EmojiConvertor();
 
-
-const ListStakes = () => {
+const ListStakes = ({bettors}) => {
     const [odd, setOdd] = useState(0)
     const [animation, setAnimation] = useState(null)
     const [data, setData] = useState({
         "status": false,
-        "stakes": [{
-            betID: 'af2e0700-37c0-4d3a-a4d3-752622956632',
-            userID: '1978772578',
-            name: '@TeamDAO???? | 3006 | APE | Edgar Perez',
-            matchID: 'EQAGTV95O7OW',
-            teamID: '0PNFSUMR3YJ9',
-            team: 'b',
-            custom: false,
-            amount: 1,
-            type: 'place-bet',
-            timestamp: '2023-01-14T12:04:10+08:00',
-            teamName: 'ASTER ARIES',
-            teams: ['INVICTUS GAMING CHINA', 'ASTER ARIES'],
-            game: '🐲DOTA',
-        }],
-    })
+        "stakes": [],
+    });
     const socket = useContext(WSContext)
+
+    useEffect(() => {
+        setData((prev) => {
+            return {
+                ...prev,
+                stakes: bettors
+            }
+        });
+        console.log(bettors);
+    }, [bettors])
+
     useEffect(() => {
         socket.on('status', (resp) => {
             const status = resp.status
@@ -205,36 +45,15 @@ const ListStakes = () => {
             })
         });
 
-        // // add mocking set data every 30 seconds
-        // const interval = setInterval(() => {
-        //     const games = ['🐲DOTA', "🔫CODM", "🐢MLBB", "🔫PUBGM"];
-        //     const fakeID = faker.database.mongodbObjectId();
-        //     const x = {
-        //         betID: fakeID,
-        //         amount : Math.floor(Math.random() * 20 + 1),
-        //         ...
-        //             randomMatch[Math.floor(Math.random() * randomMatch.length)]
-        //     };
-        //     setOdd(odd + 1);
-        //     x.odd = odd;
-        //     setAnimation(x.betID)
-        //     setData((prev) => {
-        //         return {
-        //             ...prev,
-        //             stakes: [x, ...prev.stakes].slice(0, 10)
-        //         }
-        //     })
-        // }, 5000);
-
         const timeoutId = setTimeout(() => {
             setAnimation(null);
         }, 1000);
+
 
         return () => {
             socket.off('status')
             socket.off('disconnect')
             socket.off('new_stake')
-            // clearInterval(interval)
             clearTimeout(timeoutId)
         }
     }, [data, setData, socket])
@@ -246,7 +65,7 @@ const ListStakes = () => {
                 <div className="filter-top">
                     <GameWrapper>
                         <ul>
-                            <li>All Stakes</li>
+                            <li>All</li>
                             <li>🐲DOTA</li>
                             <li>🔫CODM</li>
                             <li>🐢MLBB</li>
@@ -284,11 +103,12 @@ const ListStakes = () => {
                                 minute: 'numeric',
                                 hour12: true
                             });
-                            // make effect change by row
+                            const categoryIcon = categoryStakes.find((x) => x.name === stake.category)?.logo;
+
                             return (
                                 <tr key={index}
                                     className={`${stake.odd % 2 === 0 ? 'odd' : ''} ${animation === stake.betID ? 'animate' : ''}`}>
-                                    <td>{stake.game}</td>
+                                    <td>{`${categoryIcon} ${stake.category}`}</td>
                                     <td>{event}</td>
                                     <td className="mobile-gone">{stake.name}</td>
                                     <td className="mobile-gone">{time}</td>
