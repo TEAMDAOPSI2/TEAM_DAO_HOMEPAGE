1566b657230079fbdca301f6e124cf3b08c647e4
Aditya Rizki Pratama <arizki717@gmail.com>
2023-01-31 14:37:48 +0700

    FIX: api header and LOL playersname

--- context/WSContext.jsx
+++ context/WSContext.jsx
@@ -5,7 +5,7 @@ import {io} from "socket.io-client";
 // with header
 const socket = io("https://stake.teamdao.app/api", {
     extraHeaders: {
-        "api-key": "hjXz4mj9Tlkk5Qm6ifuA"
+        "api-key": "hjXz4mj9Tlkk5Qm6ifuAs"
     }
 })
 const WSContext = createContext(socket)
