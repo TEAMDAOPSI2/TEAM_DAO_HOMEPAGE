1ad3108b6dd5a19b70c5f26813c35bdcbaf632b9
Aditya Rizki Pratama <arizki717@gmail.com>
2023-02-02 17:08:19 +0700

    FIX: recent with own api

--- pages/stakes.jsx
+++ pages/stakes.jsx
@@ -13,7 +13,14 @@ const MainDiv = styled.div`
 
 
 export async function getServerSideProps() {
-    const res = await fetch('https://stake.teamdao.app/api/matches')
+    // with headers
+    const res = await fetch('https://stake.teamdaoapp.dev/recent', {
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'api-key': 'hjXz4mj9Tlkk5Qm6ifuAs'
+        }
+    })
     const data = await res.json()
     return {
         props: {
@@ -22,38 +29,19 @@ export async function getServerSideProps() {
     }
 }
 
-const getBettors = (data) => {
-    const bettorsToday = data.today.map(match => match.bettors).flat();
-    const bettorsYesterday = data.yesterday.map(match => match.bettors).flat();
-    const bettorsTomorrow = data.tomorrow.map(match => match.bettors).flat();
-    const bettors = [...bettorsToday, ...bettorsYesterday, ...bettorsTomorrow];
 
-    // get latest bettors
-    const newestBettors = bettors.sort((a, b) => {
-        return new Date(b.timestamp) - new Date(a.timestamp);
-    }).slice(0, 10);
-
-    const getMatchDetails = (matchID) => {
-        return data.today.find(match => match.matchID === matchID) || data.yesterday.find(match => match.matchID === matchID) || data.tomorrow.find(match => match.matchID === matchID);
-    }
-
-    return newestBettors.map((bettor, index) => {
-        const match = getMatchDetails(bettor.matchID);
-        return {
-            ...bettor,
-            teamName: match.team[bettor.team].name,
-            category: match.type,
-            teams: Object.values(match.team).map(team => team.name),
-            odd: index % 2 === 0
-        }
-    });
-
+const evenOdd = (stakes) => {
+    stakes.map((item, index) => {
+        item.odd = index + 1;
+    })
+    return stakes;
 }
+
 
 const Stakes = ({data}) => {
     const [stakes, setStakes] = useState([]);
     useEffect(() => {
-        setStakes(getBettors(data));
+        setStakes(evenOdd(data));
     }, [data]);
     return (
         <>
